d45d1a32788122c16d66554720073c116f8688ec
城ノ内2100021053 <2100021053@gs.tokushima-ec.ed.jp>
2022-01-31 08:00:29 +0900

    it is something wrong

--- file.js
+++ file.js
@@ -1,3 +1,5 @@
+let text;
+
 async function readFile(e)
 {
     let file = e.target.files[0];
@@ -7,43 +9,68 @@ async function readFile(e)
 
     reader.addEventListener("load", function()
     {
-        getReader(reader.result);
+        text = reader.result;
+        for (let i = 0; i < text.length; i++)
+        {
+            console.log(text[i]);
+        }
+        getReader();
     });
 }
 
-let notes = new Array(5);
+let data = new Array(17);
 let textCursor = 0;
-async function getReader(text)
+async function getReader()
 {
-    for (let i = 0; i < 5; i++)
+    for (let i = 0; i < data.length; i++)
     {
-        notes[i] = new Array();
+        data[i] = new Array(6);
     }
+
+    let num = 0;
+    let i = 0;
 
     while (true)
     {
-        if (text[textCursor] === "/")
+        let getc = "";
+        while (true)
         {
-            while (text[textCursor] != "\n")
+            if (text[textCursor] === "/")
+            {
+                while (text[textCursor] != "\n" && text[textCursor] != "\r")
+                {
+                    console.log(`${textCursor}: ${text[textCursor]}`);
+                    textCursor += 1;
+                }
+            }
+
+            if (text[textCursor] == "\n" || text[textCursor] == "," || text[textCursor] == "\r")
+            {
+                console.log(`${textCursor - 1}: ${getc}`);
+                data[num][i] = getc;
+                getc = "";
                 textCursor++;
-        }
+                i++;
+                console.log(getc);
+            } else {
+                // console.log(getc);
+                getc += text[textCursor];
+                textCursor++;
+            }
 
-        if (text[textCursor] == "\n")
-        {
-            textCursor++;
+            if (textCursor % data[i].length === 0)
+            {
+                // console.log("num");
+                console.log(data[i]);
+                break;
+            }
+            // console.log(data[num][i]);
         }
-
-        if (textCursor === text.length)
+        if (num >= 16)
         {
             break;
         }
-
-        for (let i = 0; i < 5; i++)
-        {
-            notes[i] = text[textCursor];
-            console.log(notes);
-        }
+        num++;
     }
 
-    console.log(notes);
 }
